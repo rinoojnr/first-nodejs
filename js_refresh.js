b01ca24961c7177987deef151dbdb3124c0d2017
rinoojnr <rinoojnr@gmail.com>
2023-11-03 13:23:32 +0530

    Refreshed the arrow functions and Objects

---
+++ js_refresh.js
@@ -0,0 +1,26 @@
+let product = (a,b) => a+b;
+console.log(product(5,20))
+
+
+
+let student = {
+    name:'Rinooj',
+    age:21,
+    class:"BCA",
+    print(){
+        console.log("My name is "+this.name)
+    }
+}
+
+student.print();
+
+
+// let myObj = {
+//     name:"Rinooj",
+//     age:25,
+//     greet(){
+//         console.log("Hi my name  is "+this.name)
+//     }
+// }
+
+// myObj.greet()
